0fdc1db7e702127faae06edaa1772ec4e7011ce3
guillebarnes <guillebarnes_95@hotmail.com>
2021-05-11 11:50:20 -0300

    Modifico nombre de algunas variables

--- dds_entrega1/src/main/java/domain/Prueba.java
+++ dds_entrega1/src/main/java/domain/Prueba.java
@@ -19,28 +19,28 @@ public class Prueba {
         CriterioClaveDebil listaDeClavesDebiles = new CriterioClaveDebil();
 
 
-        Boolean verificar = validador.esClaveValida(contraseniaNueva);
+        Boolean cumpleRequisitos = validador.esClaveValida(contraseniaNueva);
 
-        Boolean claveDebil = listaDeClavesDebiles.esClaveValida(contraseniaNueva);
+        Boolean claveFuerte = listaDeClavesDebiles.esClaveValida(contraseniaNueva);
 
 
 
-        while(!verificar || !claveDebil){
+        while(!cumpleRequisitos || !claveFuerte){
             System.out.println("Su contraseña no cumple con los requisitos, vuelva a ingresarla");
             System.out.println("Ingrese contraseña (Debe ser mayor a 8 caracteres y " +
                     "contener una mayuscula, una minuscula y un numero)");
             contrasenia = new Scanner(System.in);
             contraseniaNueva = contrasenia.nextLine();
 
-            verificar = validador.esClaveValida(contraseniaNueva);
+            cumpleRequisitos = validador.esClaveValida(contraseniaNueva);
 
-            claveDebil = listaDeClavesDebiles.esClaveValida(contraseniaNueva);
+            claveFuerte = listaDeClavesDebiles.esClaveValida(contraseniaNueva);
         }
 
-        if(verificar && claveDebil) {
+        if(cumpleRequisitos && claveFuerte) {
             Usuario user1 = new Usuario(usuarioNuevo, contraseniaNueva);
             System.out.println("Su usuario fue creado");
         }
-        
+
     }
  }
