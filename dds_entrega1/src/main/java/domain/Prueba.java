4923d6e826d12b79b03b7d04e82f798484eec064
guillebarnes <guillebarnes_95@hotmail.com>
2021-05-12 14:03:42 -0300

    Ultimos cambios en las clases

--- dds_entrega1/src/main/java/domain/Prueba.java
+++ dds_entrega1/src/main/java/domain/Prueba.java
@@ -30,33 +30,5 @@ public class Prueba {
             Usuario user1 = new Usuario(usuarioNuevo, contraseniaNueva);
             System.out.println("Su usuario fue creado");
         }
-/**
-        CriterioRegex validador = new CriterioRegex();
-        CriterioClaveDebil listaDeClavesDebiles = new CriterioClaveDebil();
-
-
-        Boolean cumpleRequisitos = validador.esClaveValida(contraseniaNueva);
-
-        Boolean claveFuerte = listaDeClavesDebiles.esClaveValida(contraseniaNueva);
-
-
-
-        while(!cumpleRequisitos || !claveFuerte){
-            System.out.println("Su contraseña no cumple con los requisitos, vuelva a ingresarla");
-            System.out.println("Ingrese contraseña (Debe ser mayor a 8 caracteres y " +
-                    "contener una mayuscula, una minuscula y un numero)");
-            contrasenia = new Scanner(System.in);
-            contraseniaNueva = contrasenia.nextLine();
-
-            cumpleRequisitos = validador.esClaveValida(contraseniaNueva);
-
-            claveFuerte = listaDeClavesDebiles.esClaveValida(contraseniaNueva);
-        }
-
-        if(cumpleRequisitos && claveFuerte) {
-            Usuario user1 = new Usuario(usuarioNuevo, contraseniaNueva);
-            System.out.println("Su usuario fue creado");
-        }
-**/
     }
  }
