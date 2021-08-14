8f57c45168bfcce34f7cb936ef2b6d02ddc92a9c
guillebarnes <guillebarnes_95@hotmail.com>
2021-08-14 10:35:25 -0300

    agrego comprotamiento a mascota

--- proyecto_patitas/src/main/java/domain/Mascota.java
+++ proyecto_patitas/src/main/java/domain/Mascota.java
@@ -4,6 +4,7 @@ import domain.entities.caracteristicas.Caracteristicas;
 import domain.entities.caracteristicas.Sexo;
 import domain.entities.caracteristicas.TipoDeMascota;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Mascota {
@@ -13,20 +14,22 @@ public class Mascota {
     private int edad;
     private Sexo sexo;
     private String descripcionFisica;
+    private Persona dueño;
     //todo Lista de fotos
-    private List<Caracteristicas> caracteristicas;
+    private List<Caracteristicas> caracteristicas = new ArrayList<>();
 
-    public Mascota(TipoDeMascota tipoDeMascota, String nombre, int edad, Sexo sexo) {
+    public Mascota(TipoDeMascota tipoDeMascota, String nombre, int edad, Sexo sexo, String apodo, Persona dueño) {
         this.tipoDeMascota = tipoDeMascota;
         this.nombre = nombre;
         this.apodo = apodo;
         this.edad = edad;
         this.sexo = sexo;
+        this.dueño = dueño;
     }
 
     public Mascota() {
     }
 
-    public void setApodo(String apodo){ this.apodo = apodo; }
+    public void agregarCaracteristica(Caracteristicas caracateristica){ this.caracteristicas.add(caracateristica); }
 
 }
