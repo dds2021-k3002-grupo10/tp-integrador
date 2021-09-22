bb9b6032dc6b2be354e62ea5a5ecd22993cd9457
guillebarnes <guillebarnes_95@hotmail.com>
2021-09-22 16:37:59 -0300

    notificaciones

--- proyecto_patitas/src/main/java/domain/notificacion/MedioDeNotificacion.java
+++ proyecto_patitas/src/main/java/domain/notificacion/MedioDeNotificacion.java
@@ -4,7 +4,7 @@ import domain.Contacto;
 
 public interface MedioDeNotificacion {
     public static final String account_sid = "ACeaa8f461078d22c1151d283f54cafc8a";
-    public static final String auth_token = "c9ab051438b9b34c802a965bbfc8739a";
+    public static final String auth_token = "87ff46721e8a12dc9ce1158e5c4a3b29";
 
     public void notificar(Contacto contacto, String mensaje);
 }
